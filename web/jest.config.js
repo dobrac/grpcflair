60ccf2f66d186b82a84f6b787844f845dd3109b6
Jakub Dobry <jakub.dobry8@gmail.com>
2024-04-18 16:05:53 +0200

    Fix coverage in CI (#4)
    * Add coverage reporters
    
    * Coverage no directory specify
    
    * Run pnpm test filter
    
    * Downgrade NodeJS to 18
    
    * Exclude coverage folder in web
    
    * Run pnpm install
    
    * Revert "Run pnpm install"
    
    This reverts commit 8c631b452dc89def77b8636611a8eb1a6adb9c93.
    
    * Frozen lockfile
    
    * Set coverage type cobertura
    
    * Test pnpm install without cache
    
    * Test pnpm install without cache
    
    * Revert "Test pnpm install without cache"
    
    This reverts commit 42e31d1e9a8573d2acf354a296466f40fe0f8682.
    
    * Upload web coverage
    
    * Change to normal test
    
    * Revert "Change to normal test"
    
    This reverts commit 878f6365ee6472ec6342b01814369cce41ead474.
    
    * Try disabling test cache
    
    * Prod false, cache tests
    
    * Try pnpm 9
    
    * Run tests serially
    
    * Revert "Try pnpm 9"
    
    This reverts commit cc6dbe67cbd38caf7d3ec5c64d26e6bf8869112b.
    
    * jest verbose
    
    * Revert "jest verbose"
    
    This reverts commit d0af7b0d75509d0ab6fa83d7cc5ae47e57acb8a3.
    
    * coverageProvider babel?

--- web/jest.config.js
+++ web/jest.config.js
@@ -21,7 +21,10 @@ const customJestConfig = {
   coveragePathIgnorePatterns: ["/node_modules/", "/tests/", "/src/contexts/"],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: "babel",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: ["json", "lcov", "text", "clover", "text", "cobertura"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
