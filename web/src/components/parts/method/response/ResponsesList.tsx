b1bdeb6ca7cf2f95d8982879a0438d276ba14449
Jakub Dobrý <jakub.dobry8@gmail.com>
2024-04-18 03:39:53 +0200

    Make responses list max height 50vh, scroll to bottom

--- web/src/components/parts/method/response/ResponsesList.tsx
+++ web/src/components/parts/method/response/ResponsesList.tsx
@@ -6,20 +6,27 @@ export default function ResponsesList({ responses }: { responses: unknown[] }) {
   }
 
   return (
-    <div className="d-grid gap-2">
-      {responses.map((response, index) => (
-        <div key={index}>
-          <JSONBlock>
-            <div className="text-secondary small">Response {index + 1}</div>
-            <JSONBlock>{JSON.stringify(response, null, 2)}</JSONBlock>
-          </JSONBlock>
+    <>
+      <div
+        className="d-flex flex-column-reverse overflow-y-auto rounded"
+        style={{
+          maxHeight: "50vh",
+        }}
+      >
+        <div className="d-grid gap-2">
+          {responses.map((response, index) => (
+            <JSONBlock key={index}>
+              <div className="text-secondary small">Response {index + 1}</div>
+              <JSONBlock>{JSON.stringify(response, null, 2)}</JSONBlock>
+            </JSONBlock>
+          ))}
         </div>
-      ))}
-      <JSONBlock>
+      </div>
+      <JSONBlock dark={false}>
         <span className="text-secondary small">
           Total responses: {responses.length}
         </span>
       </JSONBlock>
-    </div>
+    </>
   );
 }
